71d48924d91f0b984fef7f94cd5a2832fb429e25
Zach <zach@bluehousegroup.com>
2013-12-03 05:04:50 -0500

    about page styling

--- about.php
+++ about.php
@@ -9,31 +9,19 @@ if(have_posts()): while(have_posts()): the_post();
 ?>
 <section class="container">
 	<div class="row">
-		<div class="col-sm-6 col-lg-9 wysiwyg-content">
+		<h1><span>Behind the Brand</span></h1>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 wysiwyg-content">
 			<?php the_content(); ?>
-		</div>
-		<div class="col-sm-6 col-lg-3">
-			<h3>Contact Us!</h3>
-			<form role="form">
-				<fieldset>
-					<div class="form-group">
-						<label for="contact-name">Name</label>
-						<input type="text" class="text form-control" id="contact-name" name="contact-name" placeholder="Shreddy McSteez">
-					</div>
-					<div class="form-group">
-						<label for="contact-email">Email</label>
-						<input type="email" class="text email form-control" id="contact-email" name="contact-email" placeholder="skipow@allday.com">
-					</div>
-					<div class="form-group">
-						<label for="contact-message">Message</label>
-						<textarea class="textarea form-control" rows="3" placeholder="Your message here"></textarea>
-					</div>
-				</fieldset>
-			</form>
 		</div>
 	</div>
 </section>
-
+<script type="text/javascript">
+	$(function(){
+		$('.first-stop').fitVids();
+	});
+</script>
 <?php 
 endwhile;
 endif;
