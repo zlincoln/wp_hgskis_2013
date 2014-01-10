63dfc84071dbc3ddde4c8cf05f49c6ee80e9d939
Zach <zach@bluehousegroup.com>
2014-01-10 17:52:58 -0500

    adding analytics

--- footer.php
+++ footer.php
@@ -65,5 +65,18 @@
 	//@todo: set flexslider max-height dependant on window.height
 	//@todo: if html.height is less than window.height+header.height apply static-header class to body
 </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20620169-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
